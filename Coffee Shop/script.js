dab7598362be784a13b8e4aea57b2e8215600552
Fatcat0712 <maisonhai456@gmail.com>
2025-12-04 14:42:17 +0700

    code coffee shop landing page

---
+++ Coffee Shop/script.js
@@ -0,0 +1,52 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("show-mobile-menu");
+});
+
+document.addEventListener("click", (e) => {
+  const parentElement = e.target.closest(".nav-menu");
+  if (!parentElement && e.target !== menuOpenButton) {
+    document.body.classList.remove("show-mobile-menu");
+  }
+});
+
+// Initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  //   Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //   Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
